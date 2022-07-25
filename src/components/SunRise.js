383c583df345aede6a245ee13832a0fcfb3e5a90
soju <sojucr7@gmail.com>
2022-07-25 16:11:03 +0530

    cloud movement added

--- src/components/SunRise.js
+++ src/components/SunRise.js
@@ -12,8 +12,8 @@ function SunRise() {
                 <div className="sun-container">
                     <motion.div className="sun" initial={{ top: '100%' }} animate={{ top: '-100%' }} transition={{ duration: 5 }}></motion.div>
                 </div>
-                <div className="cloud"></div>
-                <div className="cloud cloud-1"></div>
+                <motion.div className="cloud"></motion.div>
+                <motion.div className="cloud cloud-1" initial={{ x: '-50vw' }} animate={{ x: '100vw' }} transition={{ duration: 50 }}></motion.div>
                 <div className="sun-container sun-container-reflection">
                     <motion.div className="sun" initial={{ top: '0%' }} animate={{ top: '150%' }} transition={{ duration: 5 }}></motion.div>
                 </div>
@@ -35,14 +35,14 @@ function SunRise() {
                 <div className="splash splash-stone delay-3"></div>
                 <div className="splash splash-stone splash-4"></div>
                 <div className="splash splash-stone splash-5"></div>
-                <div className="lotus lotus-1"></div>
-                <div className="lotus lotus-2"></div>
-                <div className="lotus lotus-3"></div>
+                <motion.div className="lotus lotus-1" style={{ transformOrigin: 'center' }} initial={{ rotate: 0 }} animate={{ rotateX:25}} transition={{ repeat: Infinity, duration: 5, repeatType: "reverse",delay: 3  }}></motion.div>
+                <motion.div className="lotus lotus-2" style={{ transformOrigin: 'center' }} initial={{ rotate: 0 }} animate={{ rotateX:30}} transition={{ repeat: Infinity, duration: 5, repeatType: "reverse",delay: 2  }}></motion.div>
+                <motion.div className="lotus lotus-3" style={{ transformOrigin: 'center' }} initial={{ rotate: 0 }} animate={{rotateX:20}} transition={{ repeat: Infinity, duration: 2, repeatType: "reverse",delay: 1  }}></motion.div>
                 <div className="front">
                     <div className="stone"></div>
                     <motion.div className="grass" style={{ transformOrigin: 'bottom' }} initial={{ rotate: 0 }} animate={{ rotate: 4 }} transition={{ ease: "linear", repeat: Infinity, duration: 5, repeatType: "reverse" }}></motion.div>
                     <motion.div className="grass grass-1" style={{ transformOrigin: 'bottom' }} initial={{ rotate: 0 }} animate={{ rotate: 4 }} transition={{ ease: "linear", repeat: Infinity, duration: 5, repeatType: "reverse" }}></motion.div>
-                    <motion.div className="grass grass-2" style={{ transformOrigin: 'bottom' }} initial={{ rotate: 0 }} animate={{ rotate: 4 }} transition={{ ease: "linear", repeat: Infinity, duration: 5, repeatType: "reverse" }}></motion.div>
+                    <motion.div className="grass grass-2" style={{ transformOrigin: 'bottom' }} initial={{ rotate: 0 }} animate={{ rotate: 8 }} transition={{ ease: "linear", repeat: Infinity, duration: 2, repeatType: "reverse" }}></motion.div>
                     <motion.div className="reed" style={{ transformOrigin: 'bottom' }} initial={{ rotate: 0 }} animate={{ rotate: 4, scaleY: 1.08 }} transition={{ repeat: Infinity, duration: 4, repeatType: "reverse" }}></motion.div>
                     <motion.div className="reed reed-1" style={{ transformOrigin: 'bottom' }} initial={{ rotate: 0 }} animate={{ rotate: 4 }} transition={{ repeat: Infinity, duration: 5, repeatType: "reverse" }}></motion.div>
                 </div>
